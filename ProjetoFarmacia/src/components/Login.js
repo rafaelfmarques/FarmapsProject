d1f119998e6c56d3111a3df61d1aa672f391db7c
Rafael Fernandes Marques <rafaelmarquesclps9@gmail.com>
2020-09-13 20:43:47 -0300

    Segundo commit

--- ProjetoFarmacia/src/components/Login.js
+++ ProjetoFarmacia/src/components/Login.js
@@ -1,45 +1,91 @@
-import React from 'react';
-import { View, Text, KeyboardAvoidingView, 
-        TextInput, Image, 
-        TouchableOpacity, StyleSheet, Animated } from 'react-native';
-
+import React, { useEffect } from 'react';
+import {
+    View, Text, KeyboardAvoidingView,
+    TextInput, Image,
+    TouchableOpacity, StyleSheet
+} from 'react-native';
+import {
+    GoogleSignin,
+    GoogleSigninButton,
+    statusCodes,
+} from '@react-native-community/google-signin'
 
 export default function Login() {
+
+    useEffect(() => {
+        GoogleSignin.configure({
+            scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+            webClientId: '124807584711-mchts3dmmvbtkgaki8vepablu26dlvt7.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+            offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+            //hostedDomain: '', // specifies a hosted domain restriction
+            //loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+            forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+            //accountName: '', // [Android] specifies an account name on the device that should be used
+            //iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+        });
+    }, [])
+    const signIn = async () => {
+        try {
+            await GoogleSignin.hasPlayServices();
+            const userInfo = await GoogleSignin.signIn();
+            //this.setState({ userInfo });
+            console.log({ userinfo })
+        } catch (error) {
+            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+                // user cancelled the login flow
+            } else if (error.code === statusCodes.IN_PROGRESS) {
+                // operation (e.g. sign in) is in progress already
+            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                // play services not available or outdated
+            } else {
+                // some other error happened
+            }
+        }
+    };
     return (
         <KeyboardAvoidingView style={styles.background}>
             <View style={styles.logo}>
-                <Image 
-                source={require('../assets/logo.png')}/>
+                <Image
+                    source={require('../assets/logo.png')} />
             </View>
 
-            <Animated.View style={[styles.container]}>
+            <View style={[styles.container]}>
                 <TextInput style={styles.input}
                     placeholder="Digite seu email"
                     autoCorrect={false}
-                    onChangeText={() => {}}
+                    onChangeText={() => { }}
                 />
 
-                <TextInput style={styles.input} 
+                <TextInput style={styles.input}
                     placeholder="Digite sua senha"
                     autoCorrect={false}
-                    onChangeText={()=>{}}
+                    onChangeText={() => { }}
                 />
+
                 <TouchableOpacity style={styles.btnAcess}>
                     <Text style={styles.btnText}>Acessar</Text>
                 </TouchableOpacity>
-            </Animated.View>
-        </KeyboardAvoidingView>  
+                <Text style={styles.text}>OU</Text>                
+                
+                <GoogleSigninButton
+                    style={{ width: 340, height: 50 }}
+                    size={GoogleSigninButton.Size.Wide}
+                    color={GoogleSigninButton.Color.Dark}
+                    /*onPress={/*this._signIn}}*/
+                    /*disabled={/*this.state.isSigninInProgress}*/ />
+            </View>
+        </KeyboardAvoidingView>
     );
 }
 
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
     background: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#c3c3c3'
     },
-    logo:{
+    logo: {
         flex: 1,
         justifyContent: 'center',
     },
@@ -47,17 +93,17 @@ const styles = StyleSheet.create ({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        width: '90%'    
+        width: '90%'
     },
-    input:{
+    input: {
         backgroundColor: '#FFF',
         width: '90%',
         marginBottom: 15,
         fontSize: 17,
         borderRadius: 7,
-        padding: 10, 
+        padding: 10,
     },
-    btnAcess:{
+    btnAcess: {
         backgroundColor: '#35AAFF',
         width: '90%',
         height: 45,
@@ -65,8 +111,12 @@ const styles = StyleSheet.create ({
         justifyContent: 'center',
         borderRadius: 7,
     },
-    btnText:{
+    btnText: {
         color: '#FFF',
         fontSize: 18,
+    },
+    text:{
+        fontSize: 20
     }
+
 })
